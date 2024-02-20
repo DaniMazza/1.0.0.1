51e424bb81ff5aaad2f3a9c06998b211fd53fda3
DaniMazza <145726469+DaniMazza@users.noreply.github.com>
2024-02-19 22:08:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const anoAtual =  new Date().getFullYear();;
+let anoNascimento = 1995;
+const idade = anoAtual - anoNascimento;
+const tamplate =  `Sua idade é ${idade} anos`;
+console.log(tamplate);
